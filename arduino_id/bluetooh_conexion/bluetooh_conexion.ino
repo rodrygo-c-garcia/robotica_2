0f6b297ae4cdf964eb2ba71b99df4cebf069cc50
Ariel Colque Garcia <colqueariel023@gmail.com>
2024-04-12 11:14:18 -0400

    feat: add file conexion bluetooh

---
+++ arduino_id/bluetooh_conexion/bluetooh_conexion.ino
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial miBT(10, 11);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("READY");
+  miBT.begin(38400);
+}
+
+void loop() {
+  if (miBT.available()){    // lee BT y envia a ARDUINO
+    Serial.write(miBT.read());
+  }
+
+  if(Serial.available()){   // lee SERIAL y envia a BT
+    miBT.write(Serial.read());
+  }
+
+}
