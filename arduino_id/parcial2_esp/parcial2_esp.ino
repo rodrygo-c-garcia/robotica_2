0f9427bce3d921db85e3faf08e10509c08a390fd
Ariel Colque Garcia <colqueariel023@gmail.com>
2024-06-06 19:00:20 -0400

    feat: add parcial2 folder and proyect

---
+++ arduino_id/parcial2_esp/parcial2_esp.ino
@@ -0,0 +1,176 @@
+// ESP82266 + HC-SR04
+#include <SoftwareSerial.h>
+#include <Ultrasonic.h>
+// 192.168.1.3
+
+#define BT_RX  10  // Pin RX para el módulo ESP8266
+#define BT_TX  11  // Pin TX para el módulo ESP8266
+
+// SENSOR ULTRASONICO
+int triggerPin = 8;
+int echoPin = 9;
+int distance = 0;
+
+bool send = false;
+String htmlContent = "";
+
+//Ultrasonic ultrasonic(trigger, echo)
+Ultrasonic ultrasonic(triggerPin, echoPin);
+
+SoftwareSerial esp8266(BT_RX, BT_TX); // RX, TX
+
+void setup() {
+  Serial.begin(9600);  // monitor serial del arduino
+  esp8266.begin(9600); // baud rate del ESP8255
+ 
+  sendComands();
+}
+
+void loop() {
+  distance = ultrasonic.read(); // retorna la distancia en cm
+  Serial.print("Distancia a Enviar: ");
+  Serial.println(distance);
+  
+  htmlContent = buildHtmlContent(String(distance));
+  delay(100);
+
+  // Verifica si hay respuesta del modulo
+  if(esp8266.available()) {
+    // verifica si hay peticion o no
+    if(esp8266.find("+IPD,")) { 
+      while(esp8266.available()){
+       char c = esp8266.read();
+       Serial.print(c);
+      }
+
+      String comandCIPSEND = "AT+CIPSEND=0," + String(htmlContent.length()) + "\r\n"; // formamos el comando para enviar el mensaje con los bytes de distancia
+      
+      sendData(comandCIPSEND,1000);
+
+      // enviamos el codigo HTML
+      esp8266.print(htmlContent);
+      Serial.println("HTML enviado");
+
+      String comandCIPCLOSE = "AT+CIPCLOSE=0\r\n";
+      sendData(comandCIPCLOSE,1000);
+
+      String comandCIPSERVER = "AT+CIPSERVER=0\r\n";
+      sendData(comandCIPSERVER,1000);
+      send = true;
+    }
+  } 
+
+  // verificamos si el HTML ha sido enviado
+  if (send) {
+    send = false;
+    sendData("AT+RST\r\n", 2000); // Resetear módulo
+    Serial.println("RESETEANDO...");
+    delay(4500);
+    sendComands();
+  }
+}
+
+/*
+Enviar comando al esp8266 y verificar la respuesta del módulo, todo esto dentro del tiempo timeout
+*/
+void sendData(String comando, const int timeout)
+{
+ long int time = millis(); // medir el tiempo actual para verificar timeout
+ 
+ esp8266.print(comando); // enviar el comando al ESP8266
+ while((time+timeout) > millis()) {
+   while(esp8266.available()) { 
+     // Leer los datos disponibles
+     char c = esp8266.read(); // leer el siguiente caracter
+     Serial.print(c);
+   }
+ } 
+ return;
+}
+
+void sendComands() {
+  sendData("AT+CIFSR\r\n",1000);    // obtener dirección IP
+  sendData("AT+CIPMUX=1\r\n",1000); // configurar para multiples conexiones
+  sendData("AT+CIPSERVER=1,80\r\n",1000);   // servidor en el puerto 80
+}
+
+// metodo para formar el codigo HTML
+String buildHtmlContent(String distance) {
+  String html = 
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/html\r\n"
+    "Connection: close\r\n"
+    "\r\n"
+    "<!DOCTYPE html>"
+    "<html>"
+    "<head>"
+    "<title>ESP8266</title>"
+    "<style>"
+    "body, html {"
+    "  display: flex;"
+    "  height: 100%;"
+    "  margin: 0;"
+    "  align-items: center;"
+    "  justify-content: center;"
+    "  background-color: #f2f2f2;" /* Gris claro */
+    "}"
+    ".container {"
+    "  display: flex;"
+    "  flex-direction: column;"
+    "  text-align: center;"
+    "}"
+    "h1 {"
+    "  color: #333;" 
+    "  font-size: 4em;"
+    "}"
+    "h2 {"
+    "  color: #666;"
+    "  font-size: 3em;"
+    "}"
+    "button {"
+    "  background-color: #4CAF50;"
+    "  color: white;"
+    "  border: none;"
+    "  padding: 20px 30px;"
+    "  border-radius: 20px;"
+    "  cursor: pointer;"
+    "  font-size: 2.2em;"
+    "}"
+    "button:disabled {"
+    "  background-color: #cccccc;" 
+    "  cursor: not-allowed;" 
+    "}"
+    "#countdown {"
+    "  font-size: 2.2em;"
+    "}"
+    "</style>"
+    "</head>"
+    "<body>"
+    "<div class='container'>"
+    "<h1>ESP8266 & HC-SRO4</h1>"
+    "<h2>Distancia: <span style='font-size: 3em;'>" + distance + " cm</span></h2>"
+    "<button id='reloadButton' onclick='reloadPage()' disabled>Solicitar</button>" 
+    "<p style='font-size: 2em;'>Solicitud disponible en <span id='countdown'></span> segundos</p>"
+    "<script>"
+    "function reloadPage() {"
+    "  location.reload();"
+    "}"
+    "var counter = 12;" 
+    "function updateCounter() {"
+    "  document.getElementById('countdown').innerText = ' ' + counter + '';" 
+    "  if (counter > 0) {" 
+    "    setTimeout(updateCounter, 1000);" 
+    "  } else {" // Si el contador llega a 0, habilitar el botón
+    "    document.getElementById('reloadButton').disabled = false;"
+    "  }"
+    "  counter--;" 
+    "}"
+    "document.addEventListener('DOMContentLoaded', function() {"
+    "  updateCounter();"
+    "});"
+    "</script>"
+    "</div>"
+    "</body>"
+    "</html>";
+  return html;
+}
